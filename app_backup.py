2e5863c0885fc4616d4dfe7d68aa9dd2a886d5e7
Tuhin <tuhinm2002.com>
2024-02-03 23:38:04 +0530

    new commit

---
+++ app_backup.py
@@ -0,0 +1,32 @@
+import streamlit as st
+from Multiapp import Multipage
+from apps import pride_and_prejudice,animal_farm,oldmansea
+from apps import of_mice_men,the_alchemist,the_universe
+
+
+def apps():
+    app = Multipage()
+    app.add_page("Pride and Prejudice", pride_and_prejudice.app)
+    app.add_page("Animal Farm",animal_farm.app)
+    app.add_page("The Alchemist",the_alchemist.app)
+    app.add_page("The Theory of Everything The Origin and Fate of the Universe",the_universe.app)
+    app.add_page("Old Man and the Sea",oldmansea.app)
+    app.add_page("Of Mice and Men", of_mice_men.app)
+    app.run()
+apps()
+st.write(" ")
+st.write(" ")
+st.write(" ")
+st.write(" ")
+st.write(" ")
+st.write(" ")
+st.write(" ")
+st.write(" ")
+st.write(" ")
+st.write(" ")
+st.write(" ")
+st.write(" ")
+st.write(" ")
+
+
+
