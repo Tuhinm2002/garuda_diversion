7f5f73792284c1e524b1752c8d25ba38e716184d
Tuhin <tuhinm2002.com>
2024-02-04 14:58:59 +0530

    new commit

--- apps/the_alchemist.py
+++ apps/the_alchemist.py
@@ -36,33 +36,36 @@ def app():
     def show_pdf(file):
         with open(file, "rb") as f:
             base64_pdf = base64.b64encode(f.read()).decode('utf-8')
-        pdf_display = F'<iframe src="data:application/pdf;base64,{base64_pdf}" width="800" height="800" type="application/pdf"></iframe>'
+        pdf_display = F'<embed src="data:application/pdf;base64,{base64_pdf}" width="800" height="800" type="application/pdf">'
         st.markdown(pdf_display, unsafe_allow_html=True)
 
     show_pdf('The_Alchemist.pdf')
 
     inp = st.text_input("input")
-    if inp is not None:
-        llm = GradientBaseModelLLM(
-            base_model_slug="llama2-7b-chat",
-            max_tokens=400
-        )
+    try:
+        if inp is not None:
+            llm = GradientBaseModelLLM(
+                base_model_slug="llama2-7b-chat",
+                max_tokens=400
+            )
 
-        embed_model = GradientEmbedding(
-            gradient_access_token=os.environ['GRADIENT_ACCESS_TOKEN'],
-            gradient_workspace_id=os.environ['GRADIENT_WORKSPACE_ID'],
-            gradient_model_slug="bge-large"
-        )
+            embed_model = GradientEmbedding(
+                gradient_access_token=os.environ['GRADIENT_ACCESS_TOKEN'],
+                gradient_workspace_id=os.environ['GRADIENT_WORKSPACE_ID'],
+                gradient_model_slug="bge-large"
+            )
 
-        service_context = ServiceContext.from_defaults(llm=llm,
-                                                       embed_model=embed_model,
-                                                       chunk_size=256
-                                                       )
-        set_global_service_context(service_context=service_context)
+            service_context = ServiceContext.from_defaults(llm=llm,
+                                                           embed_model=embed_model,
+                                                           chunk_size=256
+                                                           )
+            set_global_service_context(service_context=service_context)
 
-        storage_context = StorageContext.from_defaults(persist_dir="./The_Alchemist")
-        new_index = load_index_from_storage(storage_context=storage_context)
-        query_engine = new_index.as_query_engine()
-        response = query_engine.query(inp)
-        if response is not None:
-            st.write(response.response)
+            storage_context = StorageContext.from_defaults(persist_dir="./The_Alchemist")
+            new_index = load_index_from_storage(storage_context=storage_context)
+            query_engine = new_index.as_query_engine()
+            response = query_engine.query(inp)
+            if response is not None:
+                st.write(response.response)
+    except:
+        pass
