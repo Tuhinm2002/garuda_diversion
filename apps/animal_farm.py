8e8bc0d4a56664b2b59d866c1a5a047b00c10f09
Tuhin <tuhinm2002.com>
2024-02-04 12:01:15 +0530

    new commit

--- apps/animal_farm.py
+++ apps/animal_farm.py
@@ -14,28 +14,31 @@ def app():
 
     configure()
 
-    inp = st.text_input("input")
-    if inp is not None:
-        llm = GradientBaseModelLLM(
-            base_model_slug="llama2-7b-chat",
-            max_tokens=400
-        )
+    inp = st.text_input("input",key=101)
+    try:
+        if inp is not None:
+            llm = GradientBaseModelLLM(
+                base_model_slug="llama2-7b-chat",
+                max_tokens=400
+            )
 
-        embed_model = GradientEmbedding(
-            gradient_access_token=os.environ['GRADIENT_ACCESS_TOKEN'],
-            gradient_workspace_id=os.environ['GRADIENT_WORKSPACE_ID'],
-            gradient_model_slug="bge-large"
-        )
+            embed_model = GradientEmbedding(
+                gradient_access_token=os.environ['GRADIENT_ACCESS_TOKEN'],
+                gradient_workspace_id=os.environ['GRADIENT_WORKSPACE_ID'],
+                gradient_model_slug="bge-large"
+            )
 
-        service_context = ServiceContext.from_defaults(llm=llm,
-                                                       embed_model=embed_model,
-                                                       chunk_size=256
-                                                       )
-        set_global_service_context(service_context=service_context)
+            service_context = ServiceContext.from_defaults(llm=llm,
+                                                           embed_model=embed_model,
+                                                           chunk_size=256
+                                                           )
+            set_global_service_context(service_context=service_context)
 
-        storage_context = StorageContext.from_defaults(persist_dir="./animal_farm")
-        new_index = load_index_from_storage(storage_context=storage_context)
-        query_engine = new_index.as_query_engine()
-        response = query_engine.query(inp)
-        if response is not None:
-            st.write(response.response)
+            storage_context = StorageContext.from_defaults(persist_dir="./animal_farm")
+            new_index = load_index_from_storage(storage_context=storage_context)
+            query_engine = new_index.as_query_engine()
+            response = query_engine.query(inp)
+            if response is not None:
+                st.write(response.response)
+    except:
+        pass
